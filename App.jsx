86c022075beabacdc4f5540065f3dbd2b73ebeb2
Forever <foreverhyacinthe@gmail.com>
2026-05-15 01:44:14 +0200

    updated App.jsx

--- App.jsx
+++ App.jsx
@@ -5,11 +5,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
+  <Router>
     <Routes>
       <Route path="/login" element={<UmucoAuth />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/ai" element={<AiPage />} />
     </Routes>
+  </Router>
   );
 };
 
